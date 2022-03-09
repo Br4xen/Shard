eb3b2e9ff05f84ffa2f80ae1576f310f26b5d382
Oliver Karmetun <oliver.k@live.se>
2022-03-09 14:39:08 +0100

    Add: NPC car for debug purposes

--- ConsoleApp1/GameTest/Car.cs
+++ ConsoleApp1/GameTest/Car.cs
@@ -1,4 +1,5 @@
 ﻿using Shard;
+using System.Drawing;
 
 namespace GameTest
 {
@@ -7,9 +8,25 @@ namespace GameTest
 
         public override void initialize()
         {
+            this.Transform.X = 200.0f;
+            this.Transform.Y = 500.0f;
+            this.Transform.Z = 2.0f;
             this.Transform.SpritePath = "car.png";
 
-            //setPhysicsEnabled();
+            setPhysicsEnabled();
+
+            MyBody.Mass = 4;
+            MyBody.MaxForce = 10;
+            MyBody.AngularDrag = 0.01f;
+            MyBody.Drag = 0f;
+            MyBody.UsesGravity = false;
+            MyBody.StopOnCollision = false;
+            MyBody.ReflectOnCollision = false;
+            MyBody.ImpartForce = true;
+
+
+            MyBody.PassThrough = true;
+            MyBody.addRectCollider(0, 50, 102, 74);
 
             //MyBody.addCircleCollider(64, 64, 64);
 
@@ -29,15 +46,21 @@ namespace GameTest
 
         public void onCollisionEnter(PhysicsBody x)
         {
-
+            if (x.Parent.checkTag("Bullet") == false)
+            {
+                MyBody.DebugColor = Color.Red;
+            }
         }
 
         public void onCollisionExit(PhysicsBody x)
         {
+
+            MyBody.DebugColor = Color.Green;
         }
 
         public void onCollisionStay(PhysicsBody x)
         {
+            MyBody.DebugColor = Color.Blue;
         }
 
         public override string ToString()
